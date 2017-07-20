75f367d7786ee909a5f23b2e3523199e0a6ede58
SKIN1228 <s1620629@u.tsukuba.ac.jp>
2017-07-20 20:17:22 +0900

    諦め

---
+++ Assets/SceneLoadScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement; 
+
+public class SceneLoadScript : MonoBehaviour {
+
+	public void SceneLoad (){
+		SceneManager.LoadScene ("katamari");
+		Application.UnloadLevel ("title");
+		Resources.UnloadUnusedAssets();
+	}
+}
